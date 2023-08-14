8fd6976a2014294c8af69003a2c3b5932852dc4e
Nayem <naeemislam13790@gmail.com>
2023-08-14 13:19:12 +0600

    'commited'

--- contact_management/contact/urls.py
+++ contact_management/contact/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
 
     path('', views.contact, name = 'contact'),
     path('notes/', views.user_notes, name='user_notes'),
+    path('add-contact/',views.add_contact, name='add_contact'),
 
 ]
--- contact_management/contact/views.py
+++ contact_management/contact/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Contact,UserNote
 from django.contrib.auth.decorators import login_required
 
@@ -20,5 +20,31 @@ def contact(request):
 def user_notes(request):
     user = request.user
     notes = UserNote.objects.filter(user=user)
-    print('this is note.......',notes)
     return render(request, 'notes.html', {'notes': notes})
+
+
+def add_contact(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        address = request.POST['address']
+        # photo = request.POST['photo']
+        print(name, email, phone, address)
+        contact = Contact(name= name,email=email,phone_number=phone,
+                          address=address, user = request.user)
+        contact.save()
+        return redirect('contact')
+    return render(request, 'add.html')
+
+    
+    # if request.method == 'POST':
+    #     name = request.POST['name']
+    #     email = request.POST['email']
+    #     phone = request.POST['phone']
+    #     message = request.POST['message']
+    #     contact = Contact(name=name,email=email,phone=phone,message=message,user=request.user)
+    #     contact.save()
+    #     return redirect('contact')
+    # else:
+    #     return render(request, 'add_contact.html')
